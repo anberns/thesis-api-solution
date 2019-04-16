384065743f831783371bac4a3a6b3c0080ba4c2c
Aaron Berns <anberns@gmail.com>
2019-04-16 11:41:01 -0400

    remove show route from appointments

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :schedules, only: [:index, :show, :create, :destroy] do
-    resources :appointments, only: [:index, :show, :create, :destroy]
+    resources :appointments, only: [:index, :create, :destroy]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
