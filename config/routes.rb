f40e1b82fc32f3292ac266080f957e1adb48dffb
Aaron Berns <anberns@gmail.com>
2019-04-16 11:33:06 -0400

    add :show to allowed routes for both models

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :schedules, only: [:index, :create, :destroy] do
-    resources :appointments, only: [:index, :create, :destroy]
+  resources :schedules, only: [:index, :show, :create, :destroy] do
+    resources :appointments, only: [:index, :show, :create, :destroy]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
