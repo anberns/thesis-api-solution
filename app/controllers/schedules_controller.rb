9389c1e8ec5c3c0e0e214eab1bd3f786b2031550
Aaron Berns <anberns@gmail.com>
2019-04-16 09:36:49 -0400

    add index, show, create actions to SchedulesController

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,2 +1,25 @@
 class SchedulesController < ApplicationController
+
+  def index
+    schedules = Schedule.all
+    render json: schedules 
+  end
+
+  def show
+    schedule = Schedule.find(params[:id])
+    render json: schedule 
+  end
+
+  def create
+    schedule = Event.new(schedule_params)
+    schedules = Schedule.all
+    render json: schedules
+    end
+  end
+
+  private
+
+    def schedule_params
+      params.permit(:id)
+    end
 end
