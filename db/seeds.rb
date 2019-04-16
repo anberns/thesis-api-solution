a65f5f1fef1af5c71b2b3764d586100f4d16f947
Aaron Berns <anberns@gmail.com>
2019-04-16 09:29:59 -0400

    create db seed file for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+schedules = Schedule.create([
+  {}, {}, {}
+])
+
+appointments = Appointment.create([
+  {start_time: 1, end_time: 2, schedule_id: 1},
+  {start_time: 3, end_time: 4, schedule_id: 1},
+  {start_time: 2, end_time: 3, schedule_id: 2},
+  {start_time: 4, end_time: 5, schedule_id: 2},
+  {start_time: 3, end_time: 4, schedule_id: 3},
+  {start_time: 4, end_time: 6, schedule_id: 3},
+
+])
