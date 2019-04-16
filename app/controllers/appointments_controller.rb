547a83243f00791c24229a09f4d63678850fa5ab
Aaron Berns <anberns@gmail.com>
2019-04-16 10:47:59 -0400

    add non-nested create and destroy actions for Schedules

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -4,4 +4,20 @@ class AppointmentsController < ApplicationController
     appointments = Schedule.find(params[:schedule_id]).appointments
     render json: appointments 
   end
+
+  def create
+    schedule = Schedule.find(params[:id])
+    respond(ResponseCode::OK)
+  end
+
+  def destroy
+    Appointment.find(params[:id]).destroy 
+    respond(ResponseCode::OK)
+  end
+
+  private 
+    def appointment_params
+      params.permit(:id, :appointments [])
+    end
 end
+
--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -11,20 +11,11 @@ class SchedulesController < ApplicationController
   end
 
   def create
-    schedule = Schedule.new(schedule_params)
-    schedules = Schedule.all
-    render json: schedules
+    schedule = Schedule.create()
   end
 
   def destroy
     Schedule.find(params[:id]).destroy 
-    schedules = Schedule.all
-    render json: schedules
   end
 
-  private
-
-    def schedule_params
-      params.permit(:id, :appointments [])
-    end
 end
